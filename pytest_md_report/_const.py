7e9b1ed80df62c8833358b8ca6bc06f21e0f5263
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-05-10 13:49:31 +0900

    Redefine constants of Default

--- pytest_md_report/_const.py
+++ pytest_md_report/_const.py
@@ -1,51 +1,12 @@
 from textwrap import dedent
 
 
-class Default:
-    COLOR = "auto"
-    MARGIN = 1
-    ZEROS = "number"
-
-
 class EnvVar:
     MD_REPORT = "PYTEST_MD_REPORT"
     MD_REPORT_VERBOSE = "PYTEST_MD_REPORT_VERBOSE"
     MD_REPORT_COLOR = "PYTEST_MD_REPORT_COLOR"
     MD_REPORT_MARGIN = "PYTEST_MD_REPORT_MARGIN"
     MD_REPORT_ZEROS = "PYTEST_MD_REPORT_ZEROS"
-
-
-class HelpMsg:
-    MD_REPORT = "create markdown report."
-    MD_REPORT_VERBOSE = dedent(
-        """\
-        verbosity level for pytest-md-report. if not set, using verbosity level of pytest.
-        defaults to 0.
-        """
-    )
-    MD_REPORT_COLOR = dedent(
-        """\
-        auto: display colored (text and background) reports by using ANSI escape codes.
-        text: display colored (text) reports by using ANSI escape codes.
-        never: display report without color.
-        defaults to '{default}'.
-        """
-    ).format(default=Default.COLOR)
-    MD_REPORT_MARGIN = dedent(
-        """\
-        margin size for each cells.
-        defaults to {default}.
-        """
-    ).format(default=Default.MARGIN)
-    MD_REPORT_ZEROS = dedent(
-        """\
-        rendering method for results of zero values.
-        number: render as a digit number (0).
-        empty: not rendering.
-        defaults to {default}.
-        """
-    ).format(default=Default.ZEROS)
-    EXTRA_MSG_TEMPLATE = " you can also specify the value with {} environment variable."
 
 
 class Ini:
@@ -84,3 +45,42 @@ class FGColor:
 class BGColor:
     EVEN_ROW = "#202020"
     ODD_ROW = "black"
+
+
+class Default:
+    COLOR = ColorPoicy.AUTO
+    MARGIN = 1
+    ZEROS = ZerosRender.NUMBER
+
+
+class HelpMsg:
+    MD_REPORT = "create markdown report."
+    MD_REPORT_VERBOSE = dedent(
+        """\
+        verbosity level for pytest-md-report. if not set, using verbosity level of pytest.
+        defaults to 0.
+        """
+    )
+    MD_REPORT_COLOR = dedent(
+        """\
+        auto: display colored (text and background) reports by using ANSI escape codes.
+        text: display colored (text) reports by using ANSI escape codes.
+        never: display report without color.
+        defaults to '{default}'.
+        """
+    ).format(default=Default.COLOR)
+    MD_REPORT_MARGIN = dedent(
+        """\
+        margin size for each cells.
+        defaults to {default}.
+        """
+    ).format(default=Default.MARGIN)
+    MD_REPORT_ZEROS = dedent(
+        """\
+        rendering method for results of zero values.
+        number: render as a digit number (0).
+        empty: not rendering.
+        defaults to {default}.
+        """
+    ).format(default=Default.ZEROS)
+    EXTRA_MSG_TEMPLATE = " you can also specify the value with {} environment variable."
