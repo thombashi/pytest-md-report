5c263db2eda7e4f250fa571c4c57c9583b487e70
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-05-04 23:33:34 +0900

    Drop support for Python 3.5

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     package_data={MODULE_NAME: ["py.typed"]},
     project_urls={"Source": REPOSITORY_URL, "Tracker": "{:s}/issues".format(REPOSITORY_URL),},
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     install_requires=INSTALL_REQUIRES,
     extras_require={"test": TESTS_REQUIRES},
     classifiers=[
@@ -62,7 +62,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
