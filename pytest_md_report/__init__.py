a607b544c2f3fdae061400d13b9e2527c01082a5
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2022-08-07 02:06:03 +0900

    Add __all__

--- pytest_md_report/__init__.py
+++ pytest_md_report/__init__.py
@@ -1,3 +1,11 @@
 from .__version__ import __author__, __copyright__, __email__, __license__, __version__
 from ._const import ColorPolicy, ZerosRender
 from .plugin import make_md_report, retrieve_stat_count_map
+
+
+__all__ = (
+    "ColorPolicy",
+    "ZerosRender",
+    "make_md_report",
+    "retrieve_stat_count_map",
+)
