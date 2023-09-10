af9aed360116ad1a04e1a788b3cf9cb70fd07ff9
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-09-10 23:56:48 +0900

    Bump version

--- pytest_md_report/__version__.py
+++ pytest_md_report/__version__.py
@@ -1,6 +1,6 @@
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = f"Copyright 2020, {__author__}"
 __license__ = "MIT License"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
