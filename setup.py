7abc100a55b02df8656be7f6c68107aafa54f410
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-05-03 14:11:35 +0900

    Modify to use setuptools_scm for package build
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name=MODULE_NAME,
-    version=pkg_info["__version__"],
     url=REPOSITORY_URL,
     author=pkg_info["__author__"],
     author_email=pkg_info["__email__"],
