fd8c7da18f3e7626aa61478888a1a71b9a5d3739
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2024-05-04 00:59:43 +0900

    Bump version to 0.6.0
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- pytest_md_report/__version__.py
+++ pytest_md_report/__version__.py
@@ -1,6 +1,6 @@
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = f"Copyright 2020, {__author__}"
 __license__ = "MIT License"
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
