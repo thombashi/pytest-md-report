2c16c455d752dbfe4c2aa67d12a82629118b687c
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2025-01-02 12:09:40 +0900

    Drop support for Python 3.7/3.8
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setuptools.setup(
         "Source": REPOSITORY_URL,
         "Tracker": f"{REPOSITORY_URL:s}/issues",
     },
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     install_requires=INSTALL_REQUIRES,
     extras_require={"test": TESTS_REQUIRES},
     classifiers=[
@@ -69,8 +69,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
