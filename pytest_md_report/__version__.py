8113c0c1f70eba14b67fa09ede0b5bda0e3ff053
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2026-05-04 13:25:21 +0900

    fix: update copyright year to 2026 in __version__.py

--- pytest_md_report/__version__.py
+++ pytest_md_report/__version__.py
@@ -2,7 +2,7 @@ from typing import Final
 
 
 __author__: Final = "Tsuyoshi Hombashi"
-__copyright__: Final = f"Copyright 2020-2025, {__author__}"
+__copyright__: Final = f"Copyright 2020-2026, {__author__}"
 __license__: Final = "MIT License"
 __version__ = "0.8.0"
 __maintainer__: Final = __author__
